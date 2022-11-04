bc3a1b047dd36c0612b199ae36ca198b916ae30d
Sourav <souravkhare34@gmail.com>
2022-11-04 19:08:17 +0530

    Ability to add Contact

---
+++ AddressBookUC1.java
@@ -0,0 +1,12 @@
+package com.bridgelabz.addressbookprogram;
+
+
+public class AddressBookUC1 {
+    public static void main(String[] args) {
+        System.out.println("Welcome to Address Book");
+        Contacts obj = new Contacts("Sourav", "Khare", "Devikulam Colony", "Dewas", "MP", "455001", "8602991106", "souravkhare34@gamil.com");
+        System.out.println(obj);
+
+    }
+
+}
